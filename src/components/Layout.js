55489a4bb2a28b40dbc7af8bee15830b8e5f6e79
HinFen Kong <hfk1002@gmail.com>
2022-02-19 19:34:20 -0800

    added all elements to header bar. TODO: add images and to the buttons add funtionality

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,16 +5,22 @@ import styles from "./styles/Layout.module.css";
 function Layout() {
   return (
     <section class={styles["grid"]}>
-      <button class={styles["navBarButton"]} type="button">
-        <div class={styles["hamburgerButton"]}></div>
-        <div class={styles["hamburgerButton"]}></div>
-        <div class={styles["hamburgerButton"]}></div>
-      </button>
       <header class={styles["headerBar"]}>
+        <button class={styles["navBarButton"]} type="button">
+          <div class={styles["hamburgerButton"]}></div>
+          <div class={styles["hamburgerButton"]}></div>
+          <div class={styles["hamburgerButton"]}></div>
+        </button>
         <img class={styles["logo"]} src="" alt="logo"></img>
         <div class={styles["userNameBox"]}>
           <p class={styles["userName"]}> Username </p>
         </div>
+        <button class={styles["bellButton"]} type="button">
+          <img src="" alt="bell"></img>
+        </button>
+        <button class={styles["infoButton"]} type="button">
+          <img src="" alt="?"></img>
+        </button>
       </header>
       <nav class={styles["navBar"]}>nav</nav>
       <section class={styles["content"]}>
