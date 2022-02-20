f5b5fcbc1b9d437eb5e9fda3061b0f9856794008
HinFen Kong <hfk1002@gmail.com>
2022-02-19 20:36:18 -0800

    changed class to className

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,26 +4,26 @@ import styles from "./styles/Layout.module.css";
 
 function Layout() {
   return (
-    <section class={styles["grid"]}>
-      <header class={styles["headerBar"]}>
-        <button class={styles["navBarButton"]} type="button">
-          <div class={styles["hamburgerButton"]}></div>
-          <div class={styles["hamburgerButton"]}></div>
-          <div class={styles["hamburgerButton"]}></div>
+    <section className={styles["grid"]}>
+      <header className={styles["headerBar"]}>
+        <button className={styles["navBarButton"]} type="button">
+          <div className={styles["hamburgerButton"]}></div>
+          <div className={styles["hamburgerButton"]}></div>
+          <div className={styles["hamburgerButton"]}></div>
         </button>
-        <img class={styles["logo"]} src="" alt="logo"></img>
-        <div class={styles["userNameBox"]}>
-          <p class={styles["userName"]}> Username </p>
+        <img className={styles["logo"]} src="" alt="logo"></img>
+        <div className={styles["userNameBox"]}>
+          <p className={styles["userName"]}> Username </p>
         </div>
-        <button class={styles["bellButton"]} type="button">
+        <button className={styles["bellButton"]} type="button">
           <img src="" alt="bell"></img>
         </button>
-        <button class={styles["infoButton"]} type="button">
+        <button className={styles["infoButton"]} type="button">
           <img src="" alt="?"></img>
         </button>
       </header>
-      <nav class={styles["navBar"]}>nav</nav>
-      <section class={styles["content"]}>
+      <nav className={styles["navBar"]}>nav</nav>
+      <section className={styles["content"]}>
         <Outlet />
       </section>
     </section>
