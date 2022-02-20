6a517654561d7ea697a234f6411779a499c00de8
HinFen Kong <hfk1002@gmail.com>
2022-02-19 21:54:31 -0800

    added images to imgs folder and imported them into layout.js also added images to header bar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,25 +1,30 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import styles from "./styles/Layout.module.css";
+import hamburgerButton from "../imgs/hamburger-button.svg";
+import bell from "../imgs/bell.svg";
+import help from "../imgs/help-circle.svg";
+import grid from "../imgs/grid.svg";
+import box from "../imgs/package.svg";
+import users from "../imgs/users.svg";
+import book from "../imgs/book-open.svg";
 
 function Layout() {
   return (
     <section className={styles.grid}>
       <header className={styles.headerBar}>
         <button className={styles.navBarButton} type="button">
-          <div className={styles.hamburgerButton}></div>
-          <div className={styles.hamburgerButton}></div>
-          <div className={styles.hamburgerButton}></div>
+          <img src={hamburgerButton} alt="NavButton"></img>
         </button>
         <img className={styles.logo} src="" alt="logo"></img>
         <div className={styles.userNameBox}>
           <p className={styles.userName}> Username </p>
         </div>
         <button className={styles.bellButton} type="button">
-          <img src="" alt="bell"></img>
+          <img src={bell} alt="bell"></img>
         </button>
         <button className={styles.infoButton} type="button">
-          <img src="" alt="?"></img>
+          <img src={help} alt="?"></img>
         </button>
       </header>
       <nav className={styles.navBar}>
