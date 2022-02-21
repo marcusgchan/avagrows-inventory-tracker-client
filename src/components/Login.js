2691f11d7fb6f7538c88c41934c34439a72bed3f
Aryan <ara85@sfu.ca>
2022-02-21 13:56:17 -0800

    basic css styling applied to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import userServices from "../services/userServices";
+import styles from "./styles/Login.module.css";
 
 function Login() {
   const [username, setUsername] = useState("");
@@ -14,23 +15,30 @@ function Login() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="username">username</label>
-      <input
-        type="text"
-        name="username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <label htmlFor="password">password</label>
-      <input
-        type="text"
-        name="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button>login</button>
-    </form>
+    <body>
+        <section className={styles.loginSection}>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+                <input
+                    className={styles.textField}
+                    type="text"
+                    name="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            <label htmlFor="password">Password:</label>
+                <input
+                    className={styles.textField}
+                    type="text"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            <button>login</button>
+        </form>
+        </section>    
+    </body>
+    
   );
 }
 
