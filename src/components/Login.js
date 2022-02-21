afc9c9179ad3dab33a3f50736803f8f8a130d33f
Aryan <ara85@sfu.ca>
2022-02-21 14:52:20 -0800

    login page stylized

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import userServices from "../services/userServices";
 import styles from "./styles/Login.module.css";
+import logo from "../imgs/logo.PNG";
 
 function Login() {
   const [username, setUsername] = useState("");
@@ -16,26 +17,30 @@ function Login() {
 
   return (
     <body>
+        <section className={styles.introText}>
+            <h1>Login to the Avagrows Inventory Tracker</h1>
+            <img src={logo} className={styles.logo}></img>
+        </section>
         <section className={styles.loginSection}>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="username">Username:</label>
-                <input
-                    className={styles.textField}
-                    type="text"
-                    name="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-            <label htmlFor="password">Password:</label>
-                <input
-                    className={styles.textField}
-                    type="text"
-                    name="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            <button>login</button>
-        </form>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Username</label>
+                    <input
+                        className={styles.textField}
+                        type="text"
+                        name="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                <label htmlFor="password">Password</label>
+                    <input
+                        className={styles.textField}
+                        type="text"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                <button>login</button>
+            </form>
         </section>    
     </body>
     
