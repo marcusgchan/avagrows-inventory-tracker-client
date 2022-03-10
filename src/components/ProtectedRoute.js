05a9c9183a10529d684edc73b3b9f39bcd053d28
Marcus Chan <marcusgchan35@gmail.com>
2022-03-09 21:32:27 -0800

    fixed login

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,10 +1,11 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import React from "react";
 import useLogin from "../contexts/UserContext";
+import Login from "./Login";
 
 function ProtectedRoute() {
   const { user } = useLogin();
-  return user.id ? <Outlet /> : <Navigate to="/login" />;
+  return user.id ? <Outlet /> : <Login />;
 }
 
 export default ProtectedRoute;
