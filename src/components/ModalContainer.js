4522055aaaaf703ba5df80b7b27431c2e39beedb
Marcus Chan <marcusgchan35@gmail.com>
2022-03-11 10:36:11 -0800

    fixed modal behind table

--- src/components/ModalContainer.js
+++ src/components/ModalContainer.js
@@ -3,7 +3,9 @@ import { useNavigate } from "react-router-dom";
 import styles from "./styles/ModalContainer.module.css";
 import { useState } from "react";
 
-function ModalContainer({ children }) {
+function ModalContainer(props) {
+  console.log(props);
+  const children = props.children;
   return <section className={styles.background}>{children}</section>;
 }
 
