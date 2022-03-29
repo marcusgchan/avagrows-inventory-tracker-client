44396ccc818f39b1c67a54036bfd7a826cd836c9
dca119 <dca119@sfu.ca>
2022-03-29 16:36:55 -0700

    added table management utils and updated tableServices.js

---
+++ src/utils/partsUtils.js
@@ -0,0 +1,26 @@
+export function addParts(
+  internal_part_number,
+  part_name,
+  manufacture_name,
+  manufacture_part_number,
+  item_description,
+  unit_price,
+  line_price,
+  lead_time,
+  total_quantity,
+  category_id
+) {}
+export function deleteParts(internal_part_number) {}
+export function editParts(
+  internal_part_number,
+  old_internal_part_number,
+  part_name,
+  manufacture_name,
+  manufacture_part_number,
+  item_description,
+  unit_price,
+  line_price,
+  lead_time,
+  total_quantity,
+  category_id
+) {}
