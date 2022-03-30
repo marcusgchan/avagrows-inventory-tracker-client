07ba295adc2b6dc4ad27c6d0d10a0bf6116fa642
HinFen Kong <hfk1002@gmail.com>
2022-03-29 18:52:18 -0700

    updated urls in table services

--- src/services/tableServices.js
+++ src/services/tableServices.js
@@ -4,61 +4,73 @@ const BASE_URL = "/api";
 
 const tableServices = {
   getRows() {
-    return axios.get(BASE_URL + "/rows");
+    return axios.get(BASE_URL + "/inventory/");
   },
   getParts() {
-    return axios.get(BASE_URL + "/parts");
+    return axios.get(BASE_URL + "/parts/");
   },
   getLocations() {
-    return axios.get(BASE_URL + "/locations");
+    return axios.get(BASE_URL + "/locations/");
   },
   getStatuses() {
-    return axios.get(BASE_URL + "/statuses");
+    return axios.get(BASE_URL + "/statuses/");
   },
   getWip() {
-    return axios.get(BASE_URL + "/wip");
+    return axios.get(BASE_URL + "/wip/");
   },
   getCategories() {
-    return axios.get(BASE_URL + "/categories");
+    return axios.get(BASE_URL + "/categories/");
   },
   deletePart(row) {
-    return axios.post(BASE_URL + "/delete", row);
+    return axios.post(BASE_URL + "/inventory/delete", row);
   },
   convert(convertInfo) {
-    return axios.post(BASE_URL + "/convert", convertInfo);
+    return axios.post(BASE_URL + "/inventory/convert", convertInfo);
   },
   unconvert(unconvertInfo) {
-    return axios.post(BASE_URL + "/unconvert", unconvertInfo);
+    return axios.post(BASE_URL + "/inventory/unconvert", unconvertInfo);
   },
   changeQuantity(row) {
-    return axios.post(BASE_URL + "/changeQuantity", row);
+    return axios.post(BASE_URL + "/inventory/changeQuantity", row);
   },
   moveLocation(row) {
-    return axios.post(BASE_URL + "/moveLocation", row);
+    return axios.post(BASE_URL + "/inventory/moveLocation", row);
   },
   addPart(row) {
-    return axios.post(BASE_URL + "/addPart", row);
+    return axios.post(BASE_URL + "/inventory/addParts", row);
   },
   checkPartExists(partInfo) {
-    return axios.post(BASE_URL + "/checkPartExists", partInfo);
+    return axios.post(BASE_URL + "/parts/checkPartExists", partInfo);
   },
   addLocation(name, note, address, postalCode) {
-    return axios.post(BASE_URL + "/addLocation", name, note, address, postalCode);
+    return axios.post(
+      BASE_URL + "/locations/add",
+      name,
+      note,
+      address,
+      postalCode
+    );
   },
   deleteLocation(id) {
-    return axios.post(BASE_URL + "/deleteLocation", id);
+    return axios.post(BASE_URL + "/locations/delete", id);
   },
   editLocation(name, note, address, postalCode) {
-    return axios.post(BASE_URL + "/editLocation", name, note, address, postalCode);
+    return axios.post(
+      BASE_URL + "locations/edit",
+      name,
+      note,
+      address,
+      postalCode
+    );
   },
   addStatus(name, note) {
-    return axios.post(BASE_URL + "/addStatus", name, note);
+    return axios.post(BASE_URL + "/statuses/add", name, note);
   },
   deleteStatus(id) {
-    return axios.post(BASE_URL + "/deletStatus", id);
+    return axios.post(BASE_URL + "/statuses/delete", id);
   },
   editStatus(id, name, note) {
-    return axios.post(BASE_URL + "/editStatus", id, name, note);
+    return axios.post(BASE_URL + "/statuses/edit", id, name, note);
   },
 };
 
