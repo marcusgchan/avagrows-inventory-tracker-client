c9d856a05ff67d68feeb10d6c898f8879a6c4f47
Marcus Chan <marcusgchan35@gmail.com>
2022-04-02 13:02:12 -0700

    Refactored table component and created a custom hook for column sorting

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,150 +1,112 @@
-import { useState, useMemo, useEffect } from "react";
 import edit from "../imgs/edit.svg";
 import trash from "../imgs/trash.svg";
 import styles from "./styles/Table.module.css";
 import upDownArrow from "../imgs/up-down-arrow.svg";
 import upArrow from "../imgs/up-arrow.svg";
+import useColumnSorting from "../custom-hooks/useColumnSorting";
 
-function Table({
-  config,
-  headings,
-  rows,
-  defaultSortedHeading,
-  toggleDeleteModal,
-  toggleEditModal,
-  selectRow,
-}) {
-  function generateTableRows(rows) {
-    const headingsConfig = headings.map(({ name, type }) => ({
-      name,
-      type: type,
-    }));
-    return rows.map((row) => {
-      const id = row[config.uniqueIdProperty];
-      return (
-        <tr className={styles.dataRow} key={id.toString()}>
-          {headingsConfig.map(({ name, type }) =>
-            type === undefined ? (
-              <td key={name} className={styles.dataCell}>
-                {row[name]}
-              </td>
-            ) : (
-              <td className={styles.dataCell} key={name}>
-                <button
-                  type="button"
-                  className={styles.tableButton}
-                  onClick={() => {
-                    selectRow(id);
-                    toggleEditModal();
-                  }}
-                >
-                  <img src={edit} alt="" className={styles.tableImg}></img>
-                </button>
-                <button
-                  type="button"
-                  className={styles.tableButton}
-                  onClick={() => {
-                    selectRow(id);
-                    toggleDeleteModal();
-                  }}
-                >
-                  <img src={trash} alt="" className={styles.tableImg}></img>
-                </button>
-              </td>
-            )
-          )}
-        </tr>
-      );
-    });
-  }
-  // default sorted head and acending or not
-  const [sortedColumn, setSortedColumn] = useState({
-    activeColumn: rows[0] ? Object.keys(rows[0])[0] : "",
-    isAcending: true,
-  });
+function Table(props) {
+  const { headings, rows, defaultSortedHeading } = props;
+  const { sortedColumn, handleColumnSorting, sortedRows } = useColumnSorting(
+    rows,
+    defaultSortedHeading
+  );
 
-  useEffect(() => {
-    setSortedColumn({
-      activeColumn: defaultSortedHeading,
-      isAcending: true,
-    });
-  }, [defaultSortedHeading]);
-
-  const sortedRows = useMemo(() => {
-    const sortedArray = rows
-      .map((ele) => ({ ...ele }))
-      .sort((a, b) => {
-        const toggledColumnName = sortedColumn.activeColumn;
-        const [aVal, bVal] = [a[toggledColumnName], b[toggledColumnName]];
-
-        if (typeof aVal === "number") {
-          return sortedColumn.isAcending ? -(aVal - bVal) : aVal - bVal;
-        } else {
-          if (aVal < bVal) {
-            return sortedColumn.isAcending ? -1 : 1;
-          } else if (aVal > bVal) {
-            return sortedColumn.isAcending ? 1 : -1;
-          } else {
-            return 0;
-          }
-        }
-      });
-    return sortedArray;
-  }, [rows, sortedColumn.activeColumn, sortedColumn.isAcending]);
-
-  function handleColumnSorting(columnName) {
-    if (columnName !== sortedColumn.activeColumn) {
-      setSortedColumn({
-        activeColumn: columnName,
-        isAcending: true,
-      });
-    } else {
-      setSortedColumn({
-        ...sortedColumn,
-        isAcending: !sortedColumn.isAcending,
-      });
-    }
-  }
   return (
     <section className={styles.tableContainer}>
       <table>
         <thead className={styles.stickyHead}>
           <tr className={styles.headerRow}>
-            {headings.map(({ name, filterable }) => (
-              <th
-                className={styles.headerCell}
-                key={name}
-                onClick={
-                  filterable ? () => handleColumnSorting(name) : undefined
-                }
-              >
-                <div className={styles.headerFlex}>
-                  <span>{name.replaceAll("_", " ")}</span>
-                  {filterable && (
-                    <img
-                      src={
-                        sortedColumn.activeColumn === name
-                          ? upArrow
-                          : upDownArrow
-                      }
-                      alt=""
-                      className={`${styles.arrow} ${
-                        !sortedColumn.isAcending &&
-                        sortedColumn.activeColumn === name
-                          ? styles.rotate
-                          : ""
-                      }`}
-                    />
-                  )}
-                </div>
-              </th>
-            ))}
+            <TableHeadings
+              headings={headings}
+              sortedColumn={sortedColumn}
+              handleColumnSorting={handleColumnSorting}
+            />
           </tr>
         </thead>
-        <tbody>{generateTableRows(sortedRows)}</tbody>
+        <tbody>
+          <TableRows {...props} sortedRows={sortedRows} />
+        </tbody>
       </table>
     </section>
   );
+}
+
+function TableHeadings({ headings, sortedColumn, handleColumnSorting }) {
+  return headings.map(({ name, filterable }) => (
+    <th
+      className={styles.headerCell}
+      key={name}
+      onClick={filterable ? () => handleColumnSorting(name) : undefined}
+    >
+      <div className={styles.headerFlex}>
+        <span>{name.replaceAll("_", " ")}</span>
+        {filterable && (
+          <img
+            src={sortedColumn.activeColumn === name ? upArrow : upDownArrow}
+            alt=""
+            className={`${styles.arrow} ${
+              !sortedColumn.isAcending && sortedColumn.activeColumn === name
+                ? styles.rotate
+                : ""
+            }`}
+          />
+        )}
+      </div>
+    </th>
+  ));
+}
+
+function TableRows({
+  config,
+  headings,
+  toggleDeleteModal,
+  toggleEditModal,
+  selectRow,
+  sortedRows,
+}) {
+  const headingsConfig = headings.map(({ name, type }) => ({
+    name,
+    type: type,
+  }));
+  return sortedRows.map((row) => {
+    const id = row[config.uniqueIdProperty];
+    return (
+      <tr className={styles.dataRow} key={id.toString()}>
+        {/* Display td or action column with buttons based on type  */}
+        {headingsConfig.map(({ name, type }) =>
+          type === undefined ? (
+            <td key={name} className={styles.dataCell}>
+              {row[name]}
+            </td>
+          ) : (
+            <td className={styles.dataCell} key={name}>
+              <button
+                type="button"
+                className={styles.tableButton}
+                onClick={() => {
+                  selectRow(id);
+                  toggleEditModal();
+                }}
+              >
+                <img src={edit} alt="" className={styles.tableImg}></img>
+              </button>
+              <button
+                type="button"
+                className={styles.tableButton}
+                onClick={() => {
+                  selectRow(id);
+                  toggleDeleteModal();
+                }}
+              >
+                <img src={trash} alt="" className={styles.tableImg}></img>
+              </button>
+            </td>
+          )
+        )}
+      </tr>
+    );
+  });
 }
 
 export default Table;
