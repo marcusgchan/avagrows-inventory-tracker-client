c9adecf96c91c65a1a38b3ebeb5bf8cee5c77389
Marcus Chan <marcusgchan35@gmail.com>
2022-04-06 13:54:54 -0700

    added loader

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -2,9 +2,18 @@ import { Outlet } from "react-router-dom";
 import React from "react";
 import useLogin from "../contexts/UserContext";
 import Login from "./Login";
+import CircularProgress from "@mui/material/CircularProgress";
+import styles from "./styles/ProtectedRoute.module.css";
 
 function ProtectedRoute() {
-  const { user } = useLogin();
+  const { user, loading } = useLogin();
+  if (loading) {
+    return (
+      <div className={styles.loadingContainer}>
+        <CircularProgress />
+      </div>
+    );
+  }
   return user.id ? <Outlet /> : <Login />;
 }
 
